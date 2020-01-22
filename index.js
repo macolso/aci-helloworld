15b4f708726a0e0783f6c2d5475fdc9cc9341c65
Novin <ncnovin@gmail.com>
2020-01-21 17:51:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -33,26 +33,6 @@ app.post('/write/from/:from/to/:to', (req, res) => {
   res.redirect('/');
 })
 
-app.post('/values', (req, res) => {
-  console.log(req.body)
-
-  var filename = "/aci/logs/hamlet.txt";
-
-  fs.readFile(filename, function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    // now I need to write to emptyDir
-    fs.writeFile("/mnt/empty", data, function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log("Wrote to file");
-  });
-
-  res.redirect('/');
-})
-
 app.listen(process.env.PORT || 80, function() {
  console.log('listening on port ' + process.env.PORT || 80);
 });
